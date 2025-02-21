ec5fca38a523f0735f4860350d3d068adf27fe05
al-amin90 <ijesun30@gmail.com>
2025-02-21 16:31:07 +0600

    first commit

---
+++ eslint.config.mjs
@@ -0,0 +1,20 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  { files: ['**/*.{js,mjs,cjs,ts}'] },
+  { languageOptions: { globals: globals.browser } },
+  {
+    rules: {
+      'no-unused-vars': 'error',
+      'no-undef': 'error',
+      'prefer-const': 'error',
+      'no-console': 'warn',
+    },
+    ignores: ['dist', 'node_modules'],
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+];
---
+++ src/app.ts
@@ -0,0 +1,11 @@
+import express, { Request, Response } from 'express';
+const app = express();
+
+app.get('/', (req: Request, res: Response) => {
+  res.send({
+    status: true,
+    message: 'server is live 🎈',
+  });
+});
+
+export default app;
