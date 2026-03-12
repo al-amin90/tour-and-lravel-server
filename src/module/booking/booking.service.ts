4ed8e5243d3b2d440a407001cb8f41a969eec638
al-amin90 <ijesun30@gmail.com>
2026-03-12 23:32:11 +0600

    feat: booking create with transition rollback added

--- src/module/booking/booking.service.ts
+++ src/module/booking/booking.service.ts
@@ -1,9 +1,51 @@
+import mongoose from 'mongoose';
+import TourModal from '../tour/tour.modal';
 import { IBooking } from './booking.interface';
 import BookingModal from './booking.modal';
 
 const createBookingIntoDB = async (payload: IBooking) => {
-  const result = await BookingModal.create(payload);
-  return result;
+  const { bookedSlots, tour } = payload;
+
+  const isTourExists = await TourModal.findById(tour);
+
+  if (!isTourExists) {
+    throw new Error('Tour Not found');
+  }
+
+  payload.totalPrice = isTourExists.price * bookedSlots;
+
+  if (isTourExists.availableSeats < bookedSlots) {
+    throw new Error('Not Enough Seats Available');
+  }
+
+  const session = await mongoose.startSession();
+  session.startTransaction();
+
+  try {
+    const result = await BookingModal.create([payload], { session });
+    if (!result[0]) {
+      throw new Error('Failed to Create Booking');
+    }
+
+    const updateTour = await TourModal.findByIdAndUpdate(
+      tour,
+      { $inc: { availableSeats: -bookedSlots } },
+      { new: true, session },
+    );
+
+    if (!updateTour) {
+      throw new Error('Failed to update tour');
+    }
+
+    await session.commitTransaction();
+
+    return result[0];
+  } catch (error) {
+    await session.abortTransaction();
+    throw error;
+  } finally {
+    await session.endSession();
+  }
 };
 
 const getBookingInDB = async () => {
